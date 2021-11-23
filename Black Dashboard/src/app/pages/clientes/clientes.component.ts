a80c7be3e85757292c181f32def06e562e5ea5bb
MSteven01 <90942680+MSteven01@users.noreply.github.com>
2021-11-23 13:12:10 -0500

    Tabla Clientes

--- Black Dashboard/src/app/pages/clientes/clientes.component.ts
+++ Black Dashboard/src/app/pages/clientes/clientes.component.ts
@@ -1,5 +1,6 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { catchError, Subject, throwError } from 'rxjs';
 
 @Component({
   selector: 'app-clientes',
@@ -9,13 +10,93 @@ import { Component, OnInit } from '@angular/core';
 export class ClientesComponent implements OnInit {
 
   constructor(private objetohttp: HttpClient) { }
-  res: any
 
-  ngOnInit(): void {
+  //////////////////// GET ///////////////////
+  dtOptions: DataTables.Settings = {};
+  dtTrigger: Subject<any> = new Subject<any>();
+
+  res: any;
+  contenido: any;
+  urlapiGET: string = "http://localhost:8080/api/clientes";
+
+  handleError(error: HttpErrorResponse) {
+    let errorMessage = 'Error desconocido!';
+    if (error.error instanceof ErrorEvent) {
+      // Errores del lado del cliente
+      errorMessage = `Error: ${error.error.message}\n ${error.status}`;
+    } else {
+      // Errores del lado del servidor
+      errorMessage = `Codigo de Error: ${error.status} \nMensaje: ${error.message}`;
+    }
+    //MOSTRANDO UN ERROR EN UNA ALERTA
+    //window.alert(errorMessage);
+    return throwError(errorMessage);
   }
 
-  codigorespuesta!: number;
+  ngOnDestroy(): void {
+    this.dtTrigger.unsubscribe();
+  }
 
+  ngOnInit(): void {
+    this.res = this.objetohttp.get(this.urlapiGET).pipe
+    (catchError(this.handleError));
+
+    this.res.subscribe((datos:any[])=>{
+      this.contenido = datos;
+      console.log(this.contenido);
+      this.dtTrigger.next(this.dtOptions);
+    });
+
+    this.dtOptions = {
+      pagingType: 'full_numbers',
+      columns: [
+      {
+        title: 'Cédula',
+      }, 
+      {
+        title: 'Teléfono',
+      },
+      {
+        title: 'Nombre completo',
+      },
+      {
+        title: 'Correo electrónico',
+      },
+      {
+        title: 'Dirección',
+      }
+     ],
+      pageLength: 10,
+      responsive: true,
+      language: {
+        processing: "Procesando...",
+        search: "Buscar:",
+        lengthMenu: "Mostrar _MENU_ elementos",
+        info: "Mostrando desde _START_ al _END_ de _TOTAL_ elementos",
+        infoEmpty: "Mostrando ningún elemento.",
+        infoFiltered: "(filtrado _MAX_ elementos total)",
+        infoPostFix: "",
+        loadingRecords: "Cargando registros...",
+        zeroRecords: "No se encontraron registros",
+        emptyTable: "No hay datos disponibles en la tabla",
+        paginate: {
+          first: "Primero",
+          previous: "Anterior",
+          next: "Siguiente",
+          last: "Último"
+        },
+        aria: {
+          sortAscending: ": Activar para ordenar la tabla en orden ascendente",
+          sortDescending: ": Activar para ordenar la tabla en orden descendente"
+        }
+      }
+    };
+  }
+  
+
+  ////////////// POST /////////////////////
+
+  codigorespuesta!: number;
   cedula!: string;
   telefono!: string;
   nombrecompleto!: string;
@@ -44,8 +125,13 @@ export class ClientesComponent implements OnInit {
         }
       ).subscribe(response => {//Codigo de respuesta
         this.codigorespuesta = response.status;
-        this.codigorespuesta = 2; 
-        window.location.reload();
+        if(this.codigorespuesta == 201){
+          this.codigorespuesta = 2;
+          window.location.reload();
+        }else{
+          this.codigorespuesta = 3;
+        }
+     
       });
       
     }
